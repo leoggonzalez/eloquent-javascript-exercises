10bb0477b2c646acb77b4fd3e617c02155aace04
Leo Gonzalez <leoggonzalez@hotmail.com>
2022-04-21 23:16:44 +0200

    added bean result

--- src/public/chapter-3.js
+++ src/public/chapter-3.js
@@ -38,3 +38,36 @@ const recursionResult = document.querySelector('#recursion-result');
 recursionInput.addEventListener('change', (e) => {
   recursionResult.innerHTML = isEven(Number(e.target.value || 0)) ? 'Number is even.' : 'Number is odd.';
 })
+
+/* Bean counting */
+const beanInput1 = document.querySelector('#bean-input-1');
+const beanResult1 = document.querySelector('#bean-result-1');
+
+const charCounter = function (char) {
+  return (text) => {
+    let count = 0;
+    for (index in text) {
+      if (text[index] === char) count++;
+    }
+    return count;
+  }
+}
+
+beanInput1.addEventListener('change', (e) => {
+  const countBs = charCounter('B');
+
+  beanResult1.innerHTML = countBs(e.target.value);
+});
+
+const beanInput2 = document.querySelector('#bean-input-2');
+const beanInput3 = document.querySelector('#bean-input-3');
+const beanResult2 = document.querySelector('#bean-result-2');
+
+beanInput3.addEventListener('change', (e) => {
+  if (beanInput2.value === '') {
+    return beanResult2.innerHTML = 'Please add a character to count';
+  }
+  const countChar = charCounter(beanInput2.value);
+
+  beanResult2.innerHTML = countChar(e.target.value);
+})
