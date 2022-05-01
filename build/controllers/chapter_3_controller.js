6b5ad5c5781f1f42bcfaaaaa09fc6197cc71ec5b
Leo Gonzalez <leoggonzalez@hotmail.com>
2022-05-01 23:33:20 +0200

    removed build from repository

--- build/controllers/chapter_3_controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chapter3Controller = void 0;
-const chapter3Controller = (_, res) => {
-    res.render("chapter-3");
-};
-exports.chapter3Controller = chapter3Controller;
