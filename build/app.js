209cf626c2e87595c0ad74babd5d3df92a20b0e0
Leo Gonzalez <leoggonzalez@hotmail.com>
2022-05-01 23:26:04 +0200

    added prod

--- build/app.js
+++ build/app.js
@@ -7,15 +7,16 @@ const express_1 = __importDefault(require("express"));
 const chapter_2_controller_1 = require("./controllers/chapter_2_controller");
 const chapter_3_controller_1 = require("./controllers/chapter_3_controller");
 const path_1 = __importDefault(require("path"));
+const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use("/static", express_1.default.static(path_1.default.join(__dirname, "public")));
 app.set("view engine", "pug");
 app.set("views", "./src/views");
 app.get("/", (_, res) => {
-    res.render("index", { title: "Hey", message: "Hello there" });
+    res.render("index", { title: "Eloquen Javascript Exercises" });
 });
 app.get("/chapter-2", chapter_2_controller_1.chapter2Controller);
 app.get("/chapter-3", chapter_3_controller_1.chapter3Controller);
-app.listen(3000, () => {
-    console.log("Application is listening on port 3000!");
+app.listen(PORT, () => {
+    console.log(`Application is listening on port ${PORT}!`);
 });
